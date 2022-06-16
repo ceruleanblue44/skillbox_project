9ad01d648479cabadf639f9cbdc815226423e7ee
Marina Gotovtseva <m.gotovtseva@labmedia.su>
2022-06-16 13:38:26 +0300

    7.6 Product page

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -130,7 +130,7 @@ export default {
           .then((response) => this.productsData = response.data)
           .catch(() => this.productsLoadingFailed = true)
           .then(() => this.productsLoading = false);
-      }, 5000);
+      }, 0);
     },
   },
   watch: {
--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -1,226 +1,236 @@
 <template>
-  <main class="content container">
-    <div class="content__top">
-      <ul class="breadcrumbs">
-        <li class="breadcrumbs__item">
-          <router-link class="breadcrumbs__link" :to="{name: 'main'}"> Каталог </router-link>
-        </li>
-        <li class="breadcrumbs__item">
-          <router-link class="breadcrumbs__link" :to="{name: 'main'}"> {{ category.title }} </router-link>
-        </li>
-        <li class="breadcrumbs__item">
-          <a class="breadcrumbs__link"> {{ product.title }} </a>
-        </li>
-      </ul>
-    </div>
-
-    <section class="item">
-      <div class="item__pics pics">
-        <div class="pics__wrapper">
-          <img
-            width="570"
-            height="570"
-            :src="product.image"
-            :alt="product.title"
-          />
-        </div>
-      </div>
-
-      <div class="item__info">
-        <span class="item__code">Артикул: {{ product.id }}</span>
-        <h2 class="item__title">{{ product.title }}</h2>
-        <div class="item__form">
-          <form class="form" action="#" method="POST" @submit.prevent="addToCart">
-            <b class="item__price"> {{ product.price | numberFormat }} ₽ </b>
-
-            <fieldset class="form__block">
-              <legend class="form__legend">Цвет:</legend>
-              <ul class="colors">
-                <li class="colors__item">
-                  <label class="colors__label">
-                    <input
-                      class="colors__radio sr-only"
-                      type="radio"
-                      name="color-item"
-                      value="blue"
-                      checked=""
-                    />
-                    <span
-                      class="colors__value"
-                      style="background-color: #73b6ea"
-                    >
-                    </span>
-                  </label>
-                </li>
-                <li class="colors__item">
-                  <label class="colors__label">
-                    <input
-                      class="colors__radio sr-only"
-                      type="radio"
-                      name="color-item"
-                      value="yellow"
-                    />
-                    <span
-                      class="colors__value"
-                      style="background-color: #ffbe15"
-                    >
-                    </span>
-                  </label>
-                </li>
-                <li class="colors__item">
-                  <label class="colors__label">
-                    <input
-                      class="colors__radio sr-only"
-                      type="radio"
-                      name="color-item"
-                      value="gray" />
-                    <span
-                      class="colors__value"
-                      style="background-color: #939393"
-                    >
-                    </span
-                  ></label>
-                </li>
-              </ul>
-            </fieldset>
-
-            <fieldset class="form__block">
-              <legend class="form__legend">Объемб в ГБ:</legend>
-
-              <ul class="sizes sizes--primery">
-                <li class="sizes__item">
-                  <label class="sizes__label">
-                    <input
-                      class="sizes__radio sr-only"
-                      type="radio"
-                      name="sizes-item"
-                      value="32"
-                    />
-                    <span class="sizes__value"> 32gb </span>
-                  </label>
-                </li>
-                <li class="sizes__item">
-                  <label class="sizes__label">
-                    <input
-                      class="sizes__radio sr-only"
-                      type="radio"
-                      name="sizes-item"
-                      value="64"
-                    />
-                    <span class="sizes__value"> 64gb </span>
-                  </label>
-                </li>
-                <li class="sizes__item">
-                  <label class="sizes__label">
-                    <input
-                      class="sizes__radio sr-only"
-                      type="radio"
-                      name="sizes-item"
-                      value="128"
-                      checked=""
-                    />
-                    <span class="sizes__value"> 128gb </span>
-                  </label>
-                </li>
-              </ul>
-            </fieldset>
-
-            <div class="item__row">
-              <ProductCounter :count.sync="productAmount"/>
-              <!-- <div class="form__counter">
-                <button type="button" aria-label="Убрать один товар">
-                  <svg width="12" height="12" fill="currentColor">
-                    <use xlink:href="#icon-minus"></use>
-                  </svg>
-                </button>
-
-                <input type="text" v-model.number="productAmount" />
-
-                <button type="button" aria-label="Добавить один товар">
-                  <svg width="12" height="12" fill="currentColor">
-                    <use xlink:href="#icon-plus"></use>
-                  </svg>
-                </button>
-              </div> -->
-
-              <button class="button button--primery" type="submit">
-                В корзину
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-
-      <div class="item__desc">
-        <ul class="tabs">
-          <li class="tabs__item">
-            <a class="tabs__link tabs__link--current"> Описание </a>
+  <div>
+    <main class="content container" v-if="productLoading">Загрузка товара...</main>
+    <main class="content container" v-else-if="productLoadingFailed">Не удалось загрузить товар</main>
+    <main class="content container" v-else>
+      <div class="content__top">
+        <ul class="breadcrumbs">
+          <li class="breadcrumbs__item">
+            <router-link class="breadcrumbs__link" :to="{name: 'main'}"> Каталог </router-link>
           </li>
-          <li class="tabs__item">
-            <a class="tabs__link" href="#"> Характеристики </a>
+          <li class="breadcrumbs__item">
+            <router-link class="breadcrumbs__link" :to="{name: 'main'}"> {{ category.title }} </router-link>
           </li>
-          <li class="tabs__item">
-            <a class="tabs__link" href="#"> Гарантия </a>
-          </li>
-          <li class="tabs__item">
-            <a class="tabs__link" href="#"> Оплата и доставка </a>
+          <li class="breadcrumbs__item">
+            <a class="breadcrumbs__link"> {{ product.title }} </a>
           </li>
         </ul>
-
-        <div class="item__content">
-          <p>
-            Навигация GPS, ГЛОНАСС, BEIDOU Galileo и QZSS<br />
-            Синхронизация со смартфоном<br />
-            Связь по Bluetooth Smart, ANT+ и Wi-Fi<br />
-            Поддержка сторонних приложений<br />
-          </p>
-
-          <a href="#"> Все характеристики </a>
-          <h3>Что это?</h3>
-          <p>
-            Wahoo ELEMNT BOLT GPS – это велокомпьютер, который позволяет
-            оптимизировать свои велотренировки, сделав их максимально
-            эффективными. Wahoo ELEMNT BOLT GPSсинхронизируется с датчиками по
-            ANT+, объединяя полученную с них информацию. Данные отображаются на
-            дисплее, а также сохраняются на смартфоне. При этом на мобильное
-            устройство можно установить как фирменное приложение, так и
-            различные приложения сторонних разработчиков. Велокомпьютер точно
-            отслеживает местоположение, принимая сигнал с целого комплекса
-            спутников. Эта информация позволяет смотреть уже преодоленные
-            маршруты и планировать новые велопрогулки.
-          </p>
-          <h3>Дизайн</h3>
-          <p>
-            Велокомпьютер Wahoo ELEMNT BOLT очень компактный. Размеры устройства
-            составляют всего 74,6 x 47,3 x 22,1 мм. что не превышает габариты
-            смартфона. Корпус гаджета выполнен из черного пластика. На
-            обращенной к пользователю стороне расположен дисплей диагональю 56
-            мм. На дисплей выводятся координаты и скорость, а также полученная
-            со смартфона и синхронизированных датчиков информация:
-            интенсивность, скорость вращения педалей, пульс и т.д. (датчики не
-            входят в комплект поставки). Корпус велокомпьютера имеет степень
-            защиты от влаги IPX7. Это означает, что устройство не боится пыли, а
-            также выдерживает кратковременное (до 30 минут) погружение в воду на
-            глубину не более 1 метра.
-          </p>
-        </div>
       </div>
-    </section>
-  </main>
+
+      <section class="item">
+        <div class="item__pics pics">
+          <div class="pics__wrapper">
+            <img
+              width="570"
+              height="570"
+              :src="product.image"
+              :alt="product.title"
+            />
+          </div>
+        </div>
+
+        <div class="item__info">
+          <span class="item__code">Артикул: {{ product.id }}</span>
+          <h2 class="item__title">{{ product.title }}</h2>
+          <div class="item__form">
+            <form class="form" action="#" method="POST" @submit.prevent="addToCart">
+              <b class="item__price"> {{ product.price | numberFormat }} ₽ </b>
+
+              <fieldset class="form__block">
+                <legend class="form__legend">Цвет:</legend>
+                <ul class="colors">
+                  <li class="colors__item">
+                    <label class="colors__label">
+                      <input
+                        class="colors__radio sr-only"
+                        type="radio"
+                        name="color-item"
+                        value="blue"
+                        checked=""
+                      />
+                      <span
+                        class="colors__value"
+                        style="background-color: #73b6ea"
+                      >
+                      </span>
+                    </label>
+                  </li>
+                  <li class="colors__item">
+                    <label class="colors__label">
+                      <input
+                        class="colors__radio sr-only"
+                        type="radio"
+                        name="color-item"
+                        value="yellow"
+                      />
+                      <span
+                        class="colors__value"
+                        style="background-color: #ffbe15"
+                      >
+                      </span>
+                    </label>
+                  </li>
+                  <li class="colors__item">
+                    <label class="colors__label">
+                      <input
+                        class="colors__radio sr-only"
+                        type="radio"
+                        name="color-item"
+                        value="gray" />
+                      <span
+                        class="colors__value"
+                        style="background-color: #939393"
+                      >
+                      </span
+                    ></label>
+                  </li>
+                </ul>
+              </fieldset>
+
+              <fieldset class="form__block">
+                <legend class="form__legend">Объемб в ГБ:</legend>
+
+                <ul class="sizes sizes--primery">
+                  <li class="sizes__item">
+                    <label class="sizes__label">
+                      <input
+                        class="sizes__radio sr-only"
+                        type="radio"
+                        name="sizes-item"
+                        value="32"
+                      />
+                      <span class="sizes__value"> 32gb </span>
+                    </label>
+                  </li>
+                  <li class="sizes__item">
+                    <label class="sizes__label">
+                      <input
+                        class="sizes__radio sr-only"
+                        type="radio"
+                        name="sizes-item"
+                        value="64"
+                      />
+                      <span class="sizes__value"> 64gb </span>
+                    </label>
+                  </li>
+                  <li class="sizes__item">
+                    <label class="sizes__label">
+                      <input
+                        class="sizes__radio sr-only"
+                        type="radio"
+                        name="sizes-item"
+                        value="128"
+                        checked=""
+                      />
+                      <span class="sizes__value"> 128gb </span>
+                    </label>
+                  </li>
+                </ul>
+              </fieldset>
+
+              <div class="item__row">
+                <ProductCounter :count.sync="productAmount"/>
+                <!-- <div class="form__counter">
+                  <button type="button" aria-label="Убрать один товар">
+                    <svg width="12" height="12" fill="currentColor">
+                      <use xlink:href="#icon-minus"></use>
+                    </svg>
+                  </button>
+
+                  <input type="text" v-model.number="productAmount" />
+
+                  <button type="button" aria-label="Добавить один товар">
+                    <svg width="12" height="12" fill="currentColor">
+                      <use xlink:href="#icon-plus"></use>
+                    </svg>
+                  </button>
+                </div> -->
+
+                <button class="button button--primery" type="submit">
+                  В корзину
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div class="item__desc">
+          <ul class="tabs">
+            <li class="tabs__item">
+              <a class="tabs__link tabs__link--current"> Описание </a>
+            </li>
+            <li class="tabs__item">
+              <a class="tabs__link" href="#"> Характеристики </a>
+            </li>
+            <li class="tabs__item">
+              <a class="tabs__link" href="#"> Гарантия </a>
+            </li>
+            <li class="tabs__item">
+              <a class="tabs__link" href="#"> Оплата и доставка </a>
+            </li>
+          </ul>
+
+          <div class="item__content">
+            <p>
+              Навигация GPS, ГЛОНАСС, BEIDOU Galileo и QZSS<br />
+              Синхронизация со смартфоном<br />
+              Связь по Bluetooth Smart, ANT+ и Wi-Fi<br />
+              Поддержка сторонних приложений<br />
+            </p>
+
+            <a href="#"> Все характеристики </a>
+            <h3>Что это?</h3>
+            <p>
+              Wahoo ELEMNT BOLT GPS – это велокомпьютер, который позволяет
+              оптимизировать свои велотренировки, сделав их максимально
+              эффективными. Wahoo ELEMNT BOLT GPSсинхронизируется с датчиками по
+              ANT+, объединяя полученную с них информацию. Данные отображаются на
+              дисплее, а также сохраняются на смартфоне. При этом на мобильное
+              устройство можно установить как фирменное приложение, так и
+              различные приложения сторонних разработчиков. Велокомпьютер точно
+              отслеживает местоположение, принимая сигнал с целого комплекса
+              спутников. Эта информация позволяет смотреть уже преодоленные
+              маршруты и планировать новые велопрогулки.
+            </p>
+            <h3>Дизайн</h3>
+            <p>
+              Велокомпьютер Wahoo ELEMNT BOLT очень компактный. Размеры устройства
+              составляют всего 74,6 x 47,3 x 22,1 мм. что не превышает габариты
+              смартфона. Корпус гаджета выполнен из черного пластика. На
+              обращенной к пользователю стороне расположен дисплей диагональю 56
+              мм. На дисплей выводятся координаты и скорость, а также полученная
+              со смартфона и синхронизированных датчиков информация:
+              интенсивность, скорость вращения педалей, пульс и т.д. (датчики не
+              входят в комплект поставки). Корпус велокомпьютера имеет степень
+              защиты от влаги IPX7. Это означает, что устройство не боится пыли, а
+              также выдерживает кратковременное (до 30 минут) погружение в воду на
+              глубину не более 1 метра.
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
+  </div>
 </template>
 
+<!-- eslint-disable no-return-assign -->
 <script>
 
-import products from '@/data/products';
-import categories from '@/data/categories';
+// import products from '@/data/products';
+// import categories from '@/data/categories';
 import gotoPage from '@/helpers/gotoPage';
 import numberFormat from '@/helpers/numberFormat';
 import ProductCounter from '@/components/ProductCounter.vue';
+import axios from 'axios';
+import { API_BASE_URL } from '../config';
 
 export default {
   data() {
     return {
       productAmount: 1,
+      productData: null,
+      productLoading: false,
+      productLoadingFailed: false,
     };
   },
   components: { ProductCounter },
@@ -229,10 +239,12 @@ export default {
   },
   computed: {
     product() {
-      return products.find((product) => product.id === +this.$route.params.id);
+      // return products.find((product) => product.id === +this.$route.params.id);
+      return this.productData;
     },
     category() {
-      return categories.find((category) => category.id === this.product.categoryId);
+      // return categories.find((category) => category.id === this.product.categoryId);
+      return this.productData.category;
     },
   },
   methods: {
@@ -244,16 +256,32 @@ export default {
         { productId: this.product.id, amount: this.productAmount },
       );
     },
+    loadProduct() {
+      this.productLoading = true;
+      this.productLoadingFailed = false;
+      axios.get(`${API_BASE_URL}api/products/${this.$route.params.id}`)
+        .then((response) => this.productData = response.data)
+        .catch(() => this.productLoadingFailed = true)
+        .then(() => this.productLoading = false);
+    },
     // updateAmount() {
     //   this.productAmount = count;
     // },
   },
+  created() {
+    this.loadProduct();
+  },
   watch: {
     // This watcher if for situations when for example user puts in a non-existent id and the app breaks. Replace method lets the user come back to the working product page.
-    '$route.params.id'() {
-      if (!this.product) {
-        this.$router.replace({ name: 'notFound' });
-      }
+    '$route.params.id': {
+      handler() {
+        this.loadProduct();
+      },
+      immediate: true,
+      // The following never fires off because we have renderind conditions in the template:
+      // if (!this.product) {
+      //   this.$router.replace({ name: 'notFound' });
+      // }
     },
   },
 };
